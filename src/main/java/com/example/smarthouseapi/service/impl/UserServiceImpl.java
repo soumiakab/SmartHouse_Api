1ea3fd2df436a09ccbe1292421edb1b8148be4f0
KABRANESOUMIA <kabrane.soumia.com>
2022-04-23 16:12:37 +0000

    add jwt

--- src/main/java/com/example/smarthouseapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/smarthouseapi/service/impl/UserServiceImpl.java
@@ -4,17 +4,28 @@ import com.example.smarthouseapi.entity.Room;
 import com.example.smarthouseapi.entity.User;
 import com.example.smarthouseapi.repository.UserRepository;
 import com.example.smarthouseapi.service.interfaces.UserServiceI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserServiceI {
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserServiceI , UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public List<User> getAll(){
@@ -23,6 +34,7 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public User addUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -34,5 +46,15 @@ public class UserServiceImpl implements UserServiceI {
             return Optional.empty();
         }
         return found;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =this.userRepository.findByUserName(username);
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        //add user roles
+        if(user.getHouse()!=null)
+                authorities.add(new SimpleGrantedAuthority(user.getHouse().getId()));
+        return new org.springframework.security.core.userdetails.User(user.getUserName(),user.getPassword(),authorities);
     }
 }
